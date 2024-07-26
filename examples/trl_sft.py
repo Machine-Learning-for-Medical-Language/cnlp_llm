f0c2812e3c7a4c73ba2f60d768348726e5cbc71a
ianbulovic <ian.bulovic@gmail.com>
2024-07-26 11:09:32 -0400

    update trl_sft example and gitignore

--- examples/trl_sft.py
+++ examples/trl_sft.py
@@ -1,17 +1,21 @@
 from typing import cast
-from datasets import load_dataset, Dataset
-from trl import SFTTrainer
+
+from datasets import Dataset, load_dataset
+from trl import SFTConfig, SFTTrainer
 
 # get dataset
-dataset = cast(Dataset, load_dataset("imdb", split="train"))
+dataset = cast(
+    Dataset, load_dataset("tiny_shakespeare", split="train", trust_remote_code=True)
+)
 
-# get trainer
-trainer = SFTTrainer(
-    "facebook/opt-350m",
-    train_dataset=dataset,
+config = SFTConfig(
+    output_dir="output",
     dataset_text_field="text",
     max_seq_length=512,
 )
+
+# get trainer
+trainer = SFTTrainer("openai-community/gpt2", train_dataset=dataset, args=config)
 
 # train
 trainer.train()  # type: ignore
