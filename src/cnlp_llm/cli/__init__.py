2110ef1d46d76cd40d0b94cf5012506878ff27e4
ianbulovic <ian.bulovic@gmail.com>
2024-07-30 15:38:02 -0400

    remove inspect alias command

--- src/cnlp_llm/cli/__init__.py
+++ src/cnlp_llm/cli/__init__.py
@@ -1,6 +1,4 @@
 import click
-from inspect_ai._cli.main import inspect
-from inspect_ai._util.dotenv import init_dotenv
 
 from .chat import chat
 from .fine_tune import fine_tune
@@ -9,13 +7,20 @@ from .fine_tune import fine_tune
 @click.group()
 def cli():
     """Chat with, evaluate, and fine-tune LLMs."""
-    init_dotenv()
+    pass
+
+
+@cli.command()
+def eval():
+    """LLM evaluation."""
+    click.echo(
+        "For evaluation, use the `inspect` command.\nFor more information, see https://inspect.ai-safety-institute.org.uk/workflow.html"
+    )
 
 
 cli.add_command(chat)
-
-inspect.help = "Alias to the Inspect CLI for LLM evaluation."
-inspect.name = "evaluation"
-cli.add_command(inspect)
-
 cli.add_command(fine_tune)
+
+
+def main():
+    cli()
