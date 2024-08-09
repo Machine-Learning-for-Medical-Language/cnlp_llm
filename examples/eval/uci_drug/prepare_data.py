5aa4acf92a581655db51cfaab6e0829ef353d6c7
ianbulovic <ian.bulovic@gmail.com>
2024-08-09 10:53:02 -0400

    add uci drug example

---
+++ examples/eval/uci_drug/prepare_data.py
@@ -0,0 +1,92 @@
+"""
+Data Download Source:
+https://archive.ics.uci.edu/dataset/462/drug+review+dataset+drugs+com
+
+Data Source:
+Surya Kallumadi
+Kansas State University
+Manhattan, Kansas, USA
+surya '@' ksu.edu
+
+Felix Gräßer
+Institut für Biomedizinische Technik
+Technische Universität Dresden
+Dresden, Germany
+felix.graesser '@' tu-dresden.de
+
+
+Important Notes:
+When using this dataset, you agree that you
+1) only use the data for research purposes
+2) don't use the data for any commerical purposes
+3) don't distribute the data to anyone else
+4) cite UCI data lab and the source
+"""
+
+import sys
+import pandas as pd
+from pathlib import Path
+
+
+TRAIN_FILE = "drugsComTrain_raw.tsv"
+TEST_FILE = "drugsComTest_raw.tsv"
+
+
+def to_sentiment(rating_str: str):
+    rating = int(rating_str)
+    if rating <= 4:
+        return "Low"
+    elif rating > 4 and rating < 8:
+        return "Medium"
+    else:
+        return "High"
+
+
+def remove_newline(review: str):
+    review = review.replace("&#039;", "'")
+    review = review.replace("\n", " <cr> ")
+    review = review.replace("\r", " <cr> ")
+    review = review.replace("\t", " ")
+    return review
+
+
+def prepare_data(input_path: Path, output_path: Path):
+    # read-in files
+    df = pd.read_csv(input_path / TRAIN_FILE, sep="\t", usecols=["review", "rating"])
+    test = pd.read_csv(input_path / TEST_FILE, sep="\t", usecols=["review", "rating"])
+
+    # split into sentiments categories
+    test["target"] = test.rating.apply(to_sentiment)
+    df["target"] = df.rating.apply(to_sentiment)
+
+    # remove newlines:
+    test["review"] = test.review.apply(remove_newline)
+    df["review"] = df.review.apply(remove_newline)
+
+    # remove quotes
+    df["input"] = df["review"].str.replace('"', "")
+    test["input"] = test["review"].str.replace('"', "")
+
+    # split train and dev into 9:1 ratio
+    train = df.sample(frac=0.9, random_state=200)
+    dev = df.drop(train.index)
+
+    # select column as desired
+    test = test[["target", "input"]]
+    train = train[["target", "input"]]
+    dev = dev[["target", "input"]]
+
+    # output CSVs
+    output_path.mkdir(parents=True, exist_ok=True)
+    test.to_csv(output_path / "test.csv", index=False)
+    train.to_csv(output_path / "train.csv", index=False)
+    dev.to_csv(output_path / "dev.csv", index=False)
+
+
+if __name__ == "__main__":
+    assert (
+        len(sys.argv) == 3
+    ), "usage: `python prepare_data.py <raw dir> <processed dir>`"
+    RAW_DATA_DIR = sys.argv[1]
+    PROCESSED_DATA_DIR = sys.argv[2]
+    prepare_data(Path(RAW_DATA_DIR), Path(PROCESSED_DATA_DIR))
