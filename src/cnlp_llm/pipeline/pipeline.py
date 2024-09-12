9b62ddec5ed204f151546f70d384e5e7e2ff2aee
ianbulovic <ian.bulovic@gmail.com>
2024-09-12 10:35:19 -0400

    postprocess in call_async and add docstrings

--- src/cnlp_llm/pipeline/pipeline.py
+++ src/cnlp_llm/pipeline/pipeline.py
@@ -9,20 +9,50 @@ T = TypeVar("T")
 
 
 class Pipeline(Generic[T]):
+    """
+    An inference pipeline. This class bundles a `Plan` or list of `Solver`s
+    with an optional postprocessing step into a pipeline that can be used
+    later for inference.
+    """
+
     def __init__(
         self,
         name: str = "pipeline",
         plan: Plan | Solver | list[Solver] = generate(),
         postprocess: Callable[[EvalSample], T] = lambda x: x,
     ) -> None:
+        """Create a new pipeline.
+
+        Args:
+            name (str): The pipeline's name as it will show up in the progress indicator and eval logs. Defaults to "pipeline".
+            plan (Plan | Solver | list[Solver]): The plan for the pipeline to execute when called. Defaults to generate().
+            postprocess (Callable[[EvalSample], Any]): An optional postprocessing step that acts on resulting `EvalSample`s.
+        """
         self.name = name
         self.plan = plan
         self.postprocess = postprocess
-        self.log: EvalLog | None = None
+        self._last_eval_log: EvalLog | None = None
+
+    def get_last_eval_log(self) -> EvalLog | None:
+        """Get the most recent eval log. An eval log is generated whenever the pipeline is called.
+
+        Returns:
+            output (EvalLog | None): The most recent eval log, or `None` if this pipeline has never been called.
+        """
+        return self._last_eval_log
 
     async def call_async(
         self, data: Dataset | list[Sample] | list[str], **kwargs
-    ) -> list[EvalLog]:
+    ) -> list[T] | None:
+        """Asynchronously call this Pipeline on some data.
+
+        Args:
+            data (Dataset | list[Sample] | list[str]): The input data.
+            **kwargs (dict[str, Any]): kwargs for Inspect's `eval_async()`.
+
+        Returns:
+            output (list[T] | None): If the pipeline completes successfully, this function will return a list of postprocessed results. If the pipeline is unsuccessful, this will return `None`. You can access the log via the `get_last_eval_log()` method.
+        """
         match data:
             case Dataset():
                 dataset = data
@@ -35,16 +65,27 @@ class Pipeline(Generic[T]):
             case _:
                 raise ValueError(f"Invalid data type: {type(data)}")
 
-        return await eval_async(
+        result = await eval_async(
             Task(dataset=dataset, plan=self.plan, name=self.name), **kwargs
         )
+
+        self._last_eval_log = result[0]
+        samples = self._last_eval_log.samples
+        if samples is None:
+            return None
+
+        return [self.postprocess(sample) for sample in samples]
 
     def __call__(
         self, data: Dataset | list[Sample] | list[str], **kwargs
     ) -> list[T] | None:
-        result = asyncio.run(self.call_async(data, **kwargs))
-        self.log = result[0]
-        samples = self.log.samples
-        if samples is None:
-            return None
-        return [self.postprocess(sample) for sample in samples]
+        """Call this Pipeline on some data. This method is blocking. For asynchronous execution, use `call_async()`
+
+        Args:
+            data (Dataset | list[Sample] | list[str]): The input data.
+            **kwargs (dict[str, Any]): kwargs for Inspect's `eval_async()`.
+
+        Returns:
+            output (list[T] | None): If the pipeline completes successfully, this function will return a list of postprocessed results. If the pipeline is unsuccessful, this will return `None`. You can access the log via the `get_last_eval_log()` method.
+        """
+        return asyncio.run(self.call_async(data, **kwargs))
