778aff990bd46710d032e5a5491eb881d4db3d90
ianbulovic <ian.bulovic@gmail.com>
2024-09-12 09:56:43 -0400

    add pipeline postprocessing

--- examples/infer/pirate.py
+++ examples/infer/pirate.py
@@ -1,11 +1,7 @@
-from inspect_ai.log import EvalSample
 from inspect_ai.solver import generate, system_message
 
-from cnlp_llm.infer import Pipeline
-
-
-def extract_final_answer(sample: EvalSample):
-    return sample.messages[-1].content
+from cnlp_llm.pipeline import Pipeline
+from cnlp_llm.pipeline.postprocess import extract_final_answer
 
 
 if __name__ == "__main__":
