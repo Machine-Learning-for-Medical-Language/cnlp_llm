74605da9e94dc9c47282c4adf2ef609cd5a09f83
ianbulovic <ian.bulovic@gmail.com>
2024-10-18 09:19:00 -0400

    use mockllm model

--- src/cnlp_llm/cli/serve.py
+++ src/cnlp_llm/cli/serve.py
@@ -9,7 +9,7 @@ from inspect_ai import eval as task_eval
 from inspect_ai._cli.util import parse_cli_args
 from inspect_ai._eval.loader import load_task_spec
 from inspect_ai.dataset import MemoryDataset, Sample
-from inspect_ai.model import ModelName
+from inspect_ai.model import ModelName, get_model
 
 
 @click.command()
@@ -70,9 +70,11 @@ def serve(
     model_args = parse_cli_args(m)
     task_args = parse_cli_args(t)
 
+    model = get_model(model_name)
+
     task = load_task_spec(
         task_spec,
-        ModelName(model_name or ""),
+        ModelName(model),
         task_args=task_args,
     )[0]
 
