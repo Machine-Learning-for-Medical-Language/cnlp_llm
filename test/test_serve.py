98e5e1c5e85edabca4de350539e447315513eac0
ianbulovic <ian.bulovic@gmail.com>
2024-10-18 09:03:38 -0400

    try 127.0.0.1 as host

--- test/test_serve.py
+++ test/test_serve.py
@@ -24,7 +24,7 @@ def start_task_server(log_dir):
 @pytest.fixture(scope="session")
 def server(tmp_path_factory: TempPathFactory):
     server_log_dir = tmp_path_factory.mktemp("server_logs")
-    host = "localhost"
+    host = "127.0.0.1"
     port = "8000"
     proc = subprocess.Popen(
         [
