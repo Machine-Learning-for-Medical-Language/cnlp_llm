92ccc676a91127b34eb08210ccd2f4561a4ea2e1
ianbulovic <ian.bulovic@gmail.com>
2024-10-30 09:48:02 -0400

    add test for token counts

---
+++ test/test_count_tokens.py
@@ -0,0 +1,18 @@
+from inspect_ai.dataset import MemoryDataset, Sample
+
+from cnlp_llm.eval import get_token_counts
+
+
+def test_count_tokens():
+    n = 5
+
+    single_token = " token"
+    samples = [Sample(input=single_token * i) for i in range(1, n + 1)]
+    dataset = MemoryDataset(samples)
+
+    counts = list(get_token_counts(dataset, tiktoken_encoding_name="o200k_base"))
+
+    assert len(counts) == n
+
+    for i, count in enumerate(counts, 1):
+        assert count == i
