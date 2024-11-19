9f2b22fc899998514d2ca95c5b2c94aa5fe4c6aa
ianbulovic <ian.bulovic@gmail.com>
2024-11-19 16:29:58 -0500

    batch inference

--- src/cnlp_llm/eval/solvers/logprobs.py
+++ src/cnlp_llm/eval/solvers/logprobs.py
@@ -1,9 +1,15 @@
 import torch
-from inspect_ai.model import ChatMessage, ChatMessageAssistant, Model, get_model
+from inspect_ai.model import (
+    ChatMessage,
+    ChatMessageAssistant,
+    Model,
+    get_model,
+)
 from inspect_ai.model._providers.hf import HuggingFaceAPI
 from inspect_ai.model._providers.util.chatapi import chat_api_input
 from inspect_ai.solver import Generate, Solver, TaskState, solver
 from inspect_ai.tool import ToolInfo
+from transformers import PreTrainedModel
 
 
 @solver
@@ -14,13 +20,13 @@ def seq_prob_multiple_choice() -> Solver:
 
         model = get_model(str(state.model))
 
-        choice_probs: list[float] = []
-        for choice in state.choices:
-            choice_probs.append(
-                await get_sequence_logprob(
-                    model, state.messages + [ChatMessageAssistant(content=choice.value)]
-                )
-            )
+        choice_messages: list[ChatMessage] = [
+            ChatMessageAssistant(content=choice.value) for choice in state.choices
+        ]
+
+        choice_probs: list[float] = await get_choices_logprobs(
+            model, prefix_messages=state.messages, choices=choice_messages
+        )
 
         chosen, *rejected = [
             choice
@@ -41,35 +47,42 @@ def seq_prob_multiple_choice() -> Solver:
     return solve
 
 
-async def get_sequence_logprob(
+async def get_choices_logprobs(
     inspect_model: Model,
-    messages: list[ChatMessage],
+    prefix_messages: list[ChatMessage],
+    choices: list[ChatMessage],
     tools: list[ToolInfo] | None = None,
-) -> float:
+) -> list[float]:
     model_api = inspect_model.api
     if not isinstance(model_api, HuggingFaceAPI):
         raise ValueError(
             "Only the Huggingface API is supported for evaluating sequence probabilities."
         )
 
-    model = model_api.model
+    model: PreTrainedModel = model_api.model
     tokenizer = model_api.tokenizer
     chat_template = model_api.chat_template
 
-    # convert to hf format
-    hf_messages = chat_api_input(messages, tools or [])
+    # convert inspect ChatMessages to huggingface format
+    hf_choices_messages = [
+        chat_api_input(prefix_messages + [choice], tools or []) for choice in choices
+    ]
 
-    # apply chat template
-    input_tokens: torch.Tensor = tokenizer.apply_chat_template(
-        hf_messages,  # type: ignore
+    batched_tokens_input: torch.Tensor = tokenizer.apply_chat_template(
+        hf_choices_messages,  # type: ignore
         chat_template=chat_template,
         return_tensors="pt",
-    )
+        padding=True,
+    ).to(model.device)  # type: ignore
+
+    sequence_probabilities: list[float] = []
 
     with torch.inference_mode():
-        logits: torch.Tensor = model(input_tokens.to(model.device)).logits
-        logprobs = torch.log_softmax(logits, -1).squeeze()[:-1]
-        logprobs = logprobs[torch.arange(logprobs.shape[0]), input_tokens.squeeze()[1:]]
-        sequence_probability = logprobs.sum().item()
+        logits = model(
+            batched_tokens_input,
+        ).logits
+        logprobs = torch.log_softmax(logits, -1)[:, :-1, :]
+        logprobs = logprobs.gather(-1, batched_tokens_input[:, 1:, None]).squeeze()
+        sequence_probabilities = logprobs.sum(dim=-1).tolist()
 
-    return sequence_probability
+    return sequence_probabilities
