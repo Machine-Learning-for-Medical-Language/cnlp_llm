58b387940b6fd43c77d5b8ca198710edcb26a4f6
ianbulovic <ian.bulovic@gmail.com>
2025-02-06 09:35:35 -0500

    clean up example a bit

--- examples/eval/cola/cola_tournament.py
+++ examples/eval/cola/cola_tournament.py
@@ -1,7 +1,5 @@
 import sys
 
-from inspect_ai.dataset import Sample
-
 from cnlp_llm.cli import init_dotenv
 from cnlp_llm.eval import cnlp_dataset
 from cnlp_llm.eval.tournament import ComparisonPrompt, Tournament
@@ -10,13 +8,7 @@ from cnlp_llm.eval.tournament.scheduler import RandomScheduler
 if __name__ == "__main__":
     init_dotenv()
 
-    cola_dataset = cnlp_dataset(
-        sys.argv[1],
-        "acceptable",
-    )
-
-    def sample_to_binary(sample: Sample):
-        return sample.target == "Yes"
+    cola_dataset = cnlp_dataset(sys.argv[1], "acceptable")
 
     comparison_prompt = ComparisonPrompt(
         template='You are an expert linguist deciding whether sentences are grammatically acceptable or not. Your task is to take in a pair of sentences and decide which is more acceptable. The output format should be {"choice": <Sentence>, "reasoning": <your reasoning>}, where <Sentence> should be the more positive or less negative review, either "Sentence 1" or "Sentence 2". Here are the two sentences.\nSentence 1: {text1}\n\nSentence 2: {text2}',
@@ -27,7 +19,7 @@ if __name__ == "__main__":
     tournament = Tournament(
         model="ollama/llama3.2:1b",
         dataset=cola_dataset,
-        sample_to_binary=sample_to_binary,
+        sample_to_binary=lambda sample: sample.target == "Yes",
         comparison_prompt=comparison_prompt,
         max_players=None,
     )
