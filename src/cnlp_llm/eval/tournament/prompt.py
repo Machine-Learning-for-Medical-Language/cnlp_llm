9209c3bedd83be5f097427b84d1c5ee3a3b9bae2
ianbulovic <ian.bulovic@gmail.com>
2025-02-06 10:38:53 -0500

    fix winner extraction

--- src/cnlp_llm/eval/tournament/prompt.py
+++ src/cnlp_llm/eval/tournament/prompt.py
@@ -9,6 +9,7 @@ class ComparisonPrompt:
     template: str
     p1_indicator: str
     p2_indicator: str
+    extraction_strategy: Literal["first", "last"] = "first"
 
     def get_prompt(self, sample_1: Sample, sample_2: Sample):
         return self.template.replace("{text1}", str(sample_1.input)).replace(
@@ -16,9 +17,25 @@ class ComparisonPrompt:
         )
 
     def extract_winner(self, judgement: str) -> Literal["p1", "p2"] | None:
-        if self.p1_indicator in judgement:
-            return "p1"
-        elif self.p2_indicator in judgement:
-            return "p2"
+        if self.extraction_strategy == "first":
+            p1_idx = judgement.find(self.p1_indicator)
+            p2_idx = judgement.find(self.p2_indicator)
+
+            if p1_idx != p2_idx:
+                if p2_idx == -1 or p1_idx < p2_idx:
+                    return "p1"
+                else:
+                    return "p2"
+            else:
+                return None
         else:
-            return None
+            p1_idx = judgement.rfind(self.p1_indicator)
+            p2_idx = judgement.rfind(self.p2_indicator)
+
+            if p1_idx != p2_idx:
+                if p1_idx > p2_idx:
+                    return "p1"
+                else:
+                    return "p2"
+            else:
+                return None
