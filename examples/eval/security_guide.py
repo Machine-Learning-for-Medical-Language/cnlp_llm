ad19cba9caf678820990c6ff0afcff92bad15a23
ianbulovic <ian.bulovic@gmail.com>
2025-02-12 17:38:53 -0500

    formatting

--- examples/eval/security_guide.py
+++ examples/eval/security_guide.py
@@ -1,6 +1,5 @@
-from inspect_ai import Task
+from inspect_ai import Task, task
 from inspect_ai import eval as inspect_eval
-from inspect_ai import task
 from inspect_ai.dataset import example_dataset
 from inspect_ai.scorer import model_graded_fact
 from inspect_ai.solver import generate, system_message
