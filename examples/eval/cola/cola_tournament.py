8bbd7980ab8758d67848e6b709935a632f418e43
Tim Miller <tim.miller@gmail.com>
2025-02-20 23:42:32 -0500

    Minor change to cola tournament prompt.

--- examples/eval/cola/cola_tournament.py
+++ examples/eval/cola/cola_tournament.py
@@ -11,13 +11,13 @@ if __name__ == "__main__":
     cola_dataset = cnlp_dataset(sys.argv[1], "acceptable")
 
     comparison_prompt = ComparisonPrompt(
-        template='You are an expert linguist deciding whether sentences are grammatically acceptable or not. Your task is to take in a pair of sentences and decide which is more acceptable. The output format should be {"choice": <Sentence>, "reasoning": <your reasoning>}, where <Sentence> should be the more positive or less negative review, either "Sentence 1" or "Sentence 2". Here are the two sentences.\nSentence 1: {text1}\n\nSentence 2: {text2}',
+        template='You are an expert linguist deciding whether sentences are grammatically acceptable or not. Your task is to take in a pair of sentences and decide which is more acceptable. The output format should be {"choice": <Sentence>, "reasoning": <your reasoning>}, where <Sentence> should be the more acceptable or less unacceptable sentence, either "Sentence 1" or "Sentence 2". Here are the two sentences.\nSentence 1: {text1}\n\nSentence 2: {text2}',
         p1_indicator='"choice": "Sentence 1"',
         p2_indicator='"choice": "Sentence 2"',
     )
 
     tournament = Tournament(
-        model="ollama/llama3.2:1b",
+        model="ollama/llama3.2",
         dataset=cola_dataset,
         sample_to_binary=lambda sample: sample.target == "Yes",
         comparison_prompt=comparison_prompt,
