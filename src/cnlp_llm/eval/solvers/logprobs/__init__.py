f724274a96f3273cfdc08b4091484d1f18bac29b
ianbulovic <ian.bulovic@gmail.com>
2025-03-04 14:12:21 -0500

    add letter probs multiple choice solver

---
+++ src/cnlp_llm/eval/solvers/logprobs/__init__.py
@@ -0,0 +1,4 @@
+from .letters import letter_prob_multiple_choice
+from .sequences import seq_prob_multiple_choice
+
+__all__ = ["letter_prob_multiple_choice", "seq_prob_multiple_choice"]
--- src/cnlp_llm/eval/solvers/logprobs/common.py
+++ src/cnlp_llm/eval/solvers/logprobs/common.py
@@ -1,60 +1,12 @@
 import torch
 from inspect_ai.model import (
     ChatMessage,
-    ChatMessageAssistant,
     Model,
-    get_model,
 )
 from inspect_ai.model._providers.hf import HuggingFaceAPI
 from inspect_ai.model._providers.util.chatapi import chat_api_input
-from inspect_ai.solver import Generate, Solver, TaskState, solver
 from inspect_ai.tool import ToolInfo
 from transformers import PreTrainedModel, PreTrainedTokenizer
-
-
-@solver
-def seq_prob_multiple_choice() -> Solver:
-    async def solve(state: TaskState, generate: Generate) -> TaskState:
-        if not state.choices:
-            raise ValueError("The multiple_choice solver requires samples with choices")
-
-        model = get_model(str(state.model))
-
-        choice_messages: list[ChatMessage] = [
-            ChatMessageAssistant(content=choice.value) for choice in state.choices
-        ]
-
-        choice_probs: list[float] = await get_choices_logprobs(
-            model, prefix_messages=state.messages, choices=choice_messages
-        )
-
-        chosen, *rejected = [
-            choice
-            for choice, prob in sorted(
-                zip(state.choices, choice_probs),
-                key=lambda tup: tup[1],
-                reverse=True,
-            )
-        ]
-
-        chosen.correct = True
-        for c in rejected:
-            c.correct = False
-
-        state.messages.append(ChatMessageAssistant(content=chosen.value))
-
-        # update metadata with probabilities values
-        if "seq_probs" not in state.metadata:
-            state.metadata["seq_probs"] = []
-        seq_prob_metadata: list[dict[str, float]] = state.metadata["seq_probs"]
-        probs_map = {
-            choice.value: prob for choice, prob in zip(state.choices, choice_probs)
-        }
-        seq_prob_metadata.append(probs_map)
-
-        return state
-
-    return solve
 
 
 async def get_choices_logprobs(
