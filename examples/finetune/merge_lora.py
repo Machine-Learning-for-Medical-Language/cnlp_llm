Refactor logprobs generation for better batch inference, fix logprobs bug (#35)
* use same template as basic multiple choice

* better batching, exclude pad token from prob calculation

* add model param to seq prob solvers

* format

examples/finetune/merge_lora.py
@@ -1,19 +1,23 @@
 import sys
 
-def main(args):
 
+def main(args):
     if len(args) < 3:
-        sys.stderr.write("3 required arguments: <model> <lora dir> <merged (output) dir>\n")
+        sys.stderr.write(
+            "3 required arguments: <model> <lora dir> <merged (output) dir>\n"
+        )
         sys.exit(-1)
 
     import torch
-    from transformers import AutoModelForCausalLM, AutoTokenizer
     from peft import PeftModel
+    from transformers import AutoModelForCausalLM, AutoTokenizer
 
-    #base_model_name = "meta-llama/Llama-3.2-3B-Instruct"
+    # base_model_name = "meta-llama/Llama-3.2-3B-Instruct"
     base_model_name = args[0]
-    base_model = AutoModelForCausalLM.from_pretrained(base_model_name, torch_dtype=torch.float16)
-    #lora_adapter_path="output"
+    base_model = AutoModelForCausalLM.from_pretrained(
+        base_model_name, torch_dtype=torch.float16
+    )
+    # lora_adapter_path="output"
     lora_adapter_path = args[1]
     model = PeftModel.from_pretrained(base_model, lora_adapter_path)
     merge_model = model.merge_and_unload()
@@ -22,6 +26,6 @@ def main(args):
     tokenizer = AutoTokenizer.from_pretrained(base_model_name)
     tokenizer.save_pretrained(output_dir)
 
+
 if __name__ == "__main__":
     main(sys.argv[1:])
-